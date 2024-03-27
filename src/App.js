1413ccad4571bc8a8887bb4de045787a68e1e9da
Gowtham-2023 <gowtham.b@velocity.in>
2024-03-27 11:19:28 +0530

    feat: setup redux store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "./App.css";
 import routes from "./routes";
+import appStore from "./models/appStore";
 
 function App() {
-  return <RouterProvider router={routes} />;
+  return (
+    <Provider store={appStore}>
+      <RouterProvider router={routes} />
+    </Provider>
+  );
 }
 
 export default App;
